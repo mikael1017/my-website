3697e32d03750d1c3850780a2735c42b05c4815f
Jaewoo <jaewoocho1017@gmail.com>
2020-12-29 16:03:58 -0800

    contact Email feature added

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,11 +1,33 @@
 import React from "react";
+import emailjs from 'emailjs-com';
 
-function Contact() {
+
+
+
+export default function Contact() {
+
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm('jaewoo-gmail', 'Jaewoo Email', e.target, 'user_Z1dlJwMHA82FlM5AcegPo')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset()
+    }
+
     return (
-        <div id="contact" style={{height: 500}}>
-            contact me
-        </div>
-    )
+        <form className="contact-form" onSubmit={sendEmail}>
+          <input type="hidden" name="contact_number" />
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+        </form>
+      );
 }
-
-export default Contact;
