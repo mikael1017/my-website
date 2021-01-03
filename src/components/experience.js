42dfa8f6b715a7d91f866f4aaa0c5e8fef18027b
Jaewoo <jaewoocho1017@gmail.com>
2021-01-03 13:33:27 -0800

    changed resume link icon

--- src/components/experience.js
+++ src/components/experience.js
@@ -4,13 +4,21 @@ import "./experience.css";
 
 
 function Experience() {
+    function highlight(e) {
+        e.target.className = "fas fa-file";
+    }
+
+    function unhighlight(e) {
+        e.target.className = "far fa-file";
+    }
+
     return (
         <div id="experience" style={{height: 500}}>
             Experience
             <p>
                 <a className="resume" href={Profile} target="_blank" rel="noopener noreferrer">
                     Click to see my resume
-                    <i class="far fa-file-alt"></i>
+                    <i class="far fa-file-alt" onMouseEnter={highlight} onMouseLeave={unhighlight}></i>
                 </a>
             </p>
         </div>
--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,9 +1,15 @@
 import React from "react";
 
 function Portfolio() {
+    
+
     return (
         <div id="portfolio" style={{height: 500}}>
             portfolio
+            <p>
+                Chess game
+                -Use
+            </p>
             <p>
                 <a href="https://github.com/mikael1017" target="_blank" rel="noopener noreferrer">
                     <i class="fab fa-github"></i>
