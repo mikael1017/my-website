a52cc34515c59647af71958e0ec11ed749c5e321
Jaewoo <jaewoocho1017@gmail.com>
2021-01-06 15:51:45 -0800

    new resume added

--- src/components/experience.js
+++ src/components/experience.js
@@ -2,28 +2,35 @@ import React from "react";
 import Profile from "./document/resume.pdf";
 import "./experience.css";
 
-
 function Experience() {
-    function highlight(e) {
-        e.target.className = "fas fa-file-alt";
-    }
+  function highlight(e) {
+    e.target.className = "fas fa-file-alt";
+  }
 
-    function unhighlight(e) {
-        e.target.className = "far fa-file-alt";
-    }
+  function unhighlight(e) {
+    e.target.className = "far fa-file-alt";
+  }
 
-    return (
-        <div id="experience" style={{height: 500}}>
-            Experience
-            <p>
-                <a className="resume" href={Profile} target="_blank" rel="noopener noreferrer">
-                    Click to see my resume
-                    <i class="far fa-file-alt" onMouseEnter={highlight} onMouseLeave={unhighlight}></i>
-                </a>
-            </p>
-        </div>
-    )
+  return (
+    <div id="experience" style={{ height: 500 }}>
+      Experience
+      <p>
+        <a
+          className="resume"
+          href={Profile}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Click to see my resume
+          <i
+            class="far fa-file-alt"
+            onMouseEnter={highlight}
+            onMouseLeave={unhighlight}
+          ></i>
+        </a>
+      </p>
+    </div>
+  );
 }
-
 
 export default Experience;
