af838f167425053101f098f62ba33ebfa5fc42ff
Jaewoo <jaewoocho1017@gmail.com>
2021-01-09 21:08:08 -0800

    added experience

--- src/components/experience.js
+++ src/components/experience.js
@@ -12,8 +12,43 @@ function Experience() {
   }
 
   return (
-    <div id="experience" style={{ height: 500 }}>
-      Experience
+    <div id="experience" style={{ height: 1000 }}>
+      <h1>Experience</h1>
+
+      <div>
+        <b class="ex_title">QA Test Engineer</b>
+        <h6>
+          ● Using lab equipment(CMW/R&S), validate device application
+          features/functionality as specified in AT&T’s requirement(10776 Lab)
+          using different scenarios on simulator equipment{" "}
+        </h6>
+        <h6>
+          ● Verify device behavior for IP Messaging Evolution (IPME)
+          functionalities.
+        </h6>
+        <h6>
+          ● Verify basic IMS functionality (including error cases), VoLTE (calls
+          and supplementary services) and SMS-over-IP
+        </h6>
+        <h6>
+          ● Verify basic IMS functionality (including error cases), VoLTE (calls
+          and supplementary services) and SMS-over-IP
+        </h6>
+      </div>
+      <div>
+        <b class="ex_title">Math Tutor</b>
+        <h6 id="tutorDescription">
+          ● Tutored middle school students who struggled in math Examined what
+          each student is having trouble with and helped each individual with
+          their weaknesses Improved all of students’ grade{" "}
+        </h6>
+        <h6>
+          ● Learned how to interact with people who don’t have much
+          understanding in subject and learned how to transfer my knowledge to
+          other people
+        </h6>
+      </div>
+
       <p>
         <a
           className="resume"
@@ -21,17 +56,6 @@ function Experience() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <div>
-            <b>Math Tutor</b>
-            <h6 id="tutorDescription">
-              Tutored middle school students who struggled in math Examined what
-              each student is having trouble with and helped each individual
-              with their weaknesses Improved all of students’ grade Result:
-              Learned how to interact with people who don’t have much
-              understanding in subject and learned how to transfer my knowledge
-              to other people
-            </h6>
-          </div>
           <i
             class="far fa-file-alt"
             onMouseEnter={highlight}
