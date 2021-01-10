61b259212ab514531a02783b2cc11674bfe2684b
Jaewoo <jaewoocho1017@gmail.com>
2021-01-10 12:35:39 -0800

    added portfolio

--- src/components/experience.js
+++ src/components/experience.js
@@ -13,37 +13,37 @@ function Experience() {
 
   return (
     <div id="experience" style={{ height: 1000 }}>
-      <h1>Experience</h1>
+      <h2>Experience</h2>
 
       <div>
         <b class="ex_title">QA Test Engineer</b>
-        <h6>
-          ● Using lab equipment(CMW/R&S), validate device application
+        <h6 class="description">
+          - Using lab equipment(CMW/R&S), validate device application
           features/functionality as specified in AT&T’s requirement(10776 Lab)
           using different scenarios on simulator equipment{" "}
         </h6>
-        <h6>
-          ● Verify device behavior for IP Messaging Evolution (IPME)
+        <h6 class="description">
+          - Verify device behavior for IP Messaging Evolution (IPME)
           functionalities.
         </h6>
-        <h6>
-          ● Verify basic IMS functionality (including error cases), VoLTE (calls
+        <h6 class="description">
+          - Verify basic IMS functionality (including error cases), VoLTE (calls
           and supplementary services) and SMS-over-IP
         </h6>
-        <h6>
-          ● Verify basic IMS functionality (including error cases), VoLTE (calls
+        <h6 class="description">
+          - Verify basic IMS functionality (including error cases), VoLTE (calls
           and supplementary services) and SMS-over-IP
         </h6>
       </div>
       <div>
         <b class="ex_title">Math Tutor</b>
-        <h6 id="tutorDescription">
-          ● Tutored middle school students who struggled in math Examined what
+        <h6 class="description">
+          - Tutored middle school students who struggled in math Examined what
           each student is having trouble with and helped each individual with
           their weaknesses Improved all of students’ grade{" "}
         </h6>
-        <h6>
-          ● Learned how to interact with people who don’t have much
+        <h6 class="description">
+          - Learned how to interact with people who don’t have much
           understanding in subject and learned how to transfer my knowledge to
           other people
         </h6>
--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,22 +1,30 @@
 import React from "react";
 
 function Portfolio() {
-    
-
-    return (
-        <div id="portfolio" style={{height: 500}}>
-            portfolio
-            <p>
-                Chess game
-                -Use
-            </p>
-            <p>
-                <a href="https://github.com/mikael1017" target="_blank" rel="noopener noreferrer">
-                    <i class="fab fa-github"></i>
-                </a>
-            </p>
-        </div>
-    )
+  return (
+    <div id="portfolio" style={{ height: 500 }}>
+      <h2>Portfolio</h2>
+      <div>
+        <b class="proj_title">Chess Game</b>
+        <h6 class="description">- Used pygame library in Python</h6>
+        <h6 class="description">
+          - Written in Object Oriented Programming style
+        </h6>
+        <h6 class="description">
+          - Came up with my own algorithm of moving a chess piece
+        </h6>
+      </div>
+      <p>
+        <a
+          href="https://github.com/mikael1017"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i class="fab fa-github"></i>
+        </a>
+      </p>
+    </div>
+  );
 }
 
 export default Portfolio;
